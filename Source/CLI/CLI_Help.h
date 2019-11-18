e24c7c01e8a0837d53f110b472fd98d335c0f0f0
Jérôme Martinez <jerome@mediaarea.net>
2019-11-18 15:52:03 +0100

    Switch to permanent license in project & source files

--- Source/CLI/CLI_Help.h
+++ Source/CLI/CLI_Help.h
@@ -1,6 +1,6 @@
 /*  Copyright (c) MIPoPS. All Rights Reserved.
  *
- *  Use of this source code is governed by a Unlicense license that can
+ *  Use of this source code is governed by a BSD-3-Clause license that can
  *  be found in the LICENSE file in the root of the source tree.
  */
 
