f60b642c9ea584c12831e97d569c4b36192cfcaa
Jérôme Martinez <jerome@mediaarea.net>
2021-08-22 23:11:21 +0200

    Bump XML document version to 1.2

--- Source/Common/Output_Xml.cpp
+++ Source/Common/Output_Xml.cpp
@@ -105,7 +105,7 @@ return_value Output_Xml(ostream& Out, std::vector<file*>& PerFile, bitset<Option
 
     // XML header
     Text += "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
-        "<dvrescue xmlns=\"https://mediaarea.net/dvrescue\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"https://mediaarea.net/dvrescue https://mediaarea.net/dvrescue/dvrescue.xsd\" version=\"1.1\">\n"
+        "<dvrescue xmlns=\"https://mediaarea.net/dvrescue\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"https://mediaarea.net/dvrescue https://mediaarea.net/dvrescue/dvrescue.xsd\" version=\"1.2\">\n"
         "\t<creator>\n"
         "\t\t<program>dvrescue</program>\n"
         "\t\t<version>" Program_Version "</version>\n"
