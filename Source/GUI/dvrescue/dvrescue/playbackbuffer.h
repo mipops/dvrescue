fa919459f6768bea16b6e3874d23a37334e52946
Alexander Ivash <elderorb@gmail.com>
2022-01-14 21:54:49 +0530

    do not crash on big files

--- Source/GUI/dvrescue/dvrescue/playbackbuffer.h
+++ Source/GUI/dvrescue/dvrescue/playbackbuffer.h
@@ -4,48 +4,71 @@
 #include <QIODevice>
 #include <QMutex>
 #include <QObject>
+#include <QDebug>
+#include <QWaitCondition>
 
 class Buffer: public QIODevice
 {
 public:
     qint64 readData(char *data, qint64 maxSize) override
     {
+        // qDebug() << "readData: " << maxSize;
         QMutexLocker locker(&m_mutex);
         if (!maxSize)
             return 0;
 
-        QByteArray ba = m_buffer.mid(m_pos, maxSize);
-        memcpy(data, ba.data(), ba.size());
-        m_pos += ba.size();
-        return ba.size();
+        // qDebug() << "m_buffer.size = " << m_buffer.size();
+        auto sizeToCopy = qMin(maxSize, (qint64) m_buffer.size());
+        memcpy(data, m_buffer.data() + m_pos, sizeToCopy);
+
+        m_buffer.remove(m_pos, sizeToCopy);
+
+        // qDebug() << "m_buffer.size = " << m_buffer.size();
+
+        if(m_buffer.size() < 1024 * 1024) {
+            // qDebug() << "buffer size is: " << m_buffer.size() << ", awaking writer";
+            m_bufferNotFull.wakeAll();
+        }
+
+        // qDebug() << "readData: data read, buffer: " << ba.size();
+        m_bytesRead += sizeToCopy;
+        // qDebug() << "readData: data read, total read: " << m_bytesRead;
+
+        return sizeToCopy;
     }
 
     qint64 writeData(const char *data, qint64 maxSize) override
     {
+        // qDebug() << "writeData: " << maxSize;
         QMutexLocker locker(&m_mutex);
+        if(m_buffer.size() > 1024 * 1024) {
+            // qDebug() << "writeData: wait on buffer not full";
+            m_bufferNotFull.wait(&m_mutex);
+            // qDebug() << "writeData: buffer not full";
+        }
+
+        // qDebug() << "before append: " << m_buffer.size() << ba.size();
         QByteArray ba(data, maxSize);
         m_buffer.append(ba);
+        // qDebug() << "m_buffer: " << m_buffer.size();
+
+        m_bytesWritten += ba.size();
+        // qDebug() << "writeData: total data written: " << m_bytesWritten;
         return ba.size();
     }
 
     bool atEnd() const override
     {
         return false;
-
-        QMutexLocker locker(&m_mutex);
-        return m_pos >= m_size;
     }
 
     qint64 pos() const override
     {
-        QMutexLocker locker(&m_mutex);
-        return m_pos;
+        return 0;
     }
 
     bool seek(qint64 pos) override
     {
-        QMutexLocker locker(&m_mutex);
-        m_pos = pos;
         return true;
     }
 
@@ -58,6 +81,9 @@ public:
     qint64 m_pos = 0;
     QByteArray m_buffer;
     mutable QMutex m_mutex;
+    QWaitCondition m_bufferNotFull;
+    quint64 m_bytesRead { 0 };
+    quint64 m_bytesWritten { 0 };
 };
 
 class BufferSequential : public Buffer
