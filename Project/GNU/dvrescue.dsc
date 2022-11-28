c6929eec82891c071243aeae2cd9c0fb88c8a160
Maxime Gervais <gervais.maxime@gmail.com>
2022-11-28 10:47:22 +0100

    Capture support for Linux
    Signed-off-by: Maxime Gervais <gervais.maxime@gmail.com>

--- Project/GNU/dvrescue.dsc
+++ Project/GNU/dvrescue.dsc
@@ -6,7 +6,7 @@ Version: 0.21.11-1
 Maintainer: Jerome Martinez <Info@MediaArea.net>
 Homepage: https://MediaArea.net
 Standards-Version: 3.7.3
-Build-Depends: debhelper (>= 5), libmediainfo-dev, libzen-dev, zlib1g-dev, pkg-config, automake, autoconf, libtool
+Build-Depends: debhelper (>= 5), libmediainfo-dev, libzen-dev, libraw1394-dev, libavc1394-dev, libiec61883-dev, zlib1g-dev, pkg-config, automake, autoconf, libtool
 Files:
  00000000000000000000000000000000 000000 dvrescue_0.21.11.orig.tar.xz
  00000000000000000000000000000000 000000 dvrescue_0.21.11-1.debian.tar.xz
---
+++ Source/Common/LinuxWrapper.h
@@ -0,0 +1,85 @@
+/*  Copyright (c) MIPoPS. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-3-Clause license that can
+ *  be found in the LICENSE.txt file in the root of the source tree.
+ */
+
+//---------------------------------------------------------------------------
+#pragma once
+
+#include "ZenLib/Ztring.h"
+#include <exception>
+#include <atomic>
+#include <string>
+#include <thread>
+#include <mutex>
+
+#include "Common/ProcessFileWrapper.h"
+
+#include <libraw1394/raw1394.h>
+#include <libiec61883/iec61883.h>
+
+//***************************************************************************
+// Class SimulatorWrapper
+//***************************************************************************
+
+class LinuxWrapper : public BaseWrapper {
+ public:
+    // Types
+    class error : public std::exception
+    {
+    public:
+        error(std::string message) : std::exception(), message(message) {};
+        virtual const char* what() const throw() { return message.c_str(); }
+    private:
+        std::string message;
+    };
+
+    struct device
+    {
+        int Port;
+        nodeid_t Node;
+        octlet_t UUID;
+        std::string Vendor;
+        std::string Model;
+
+        device(int Port, nodeid_t Node, octlet_t UUID, std::string Vendor, std::string Model)
+        : Port(Port), Node(Node), UUID(UUID), Vendor(Vendor), Model(Model) {};
+    };
+
+    // Constructor/Destructor
+    LinuxWrapper(std::size_t DeviceIndex);
+    ~LinuxWrapper();
+
+    // Functions
+    static void Init();
+    static std::size_t GetDeviceCount();
+    static std::string GetDeviceName(std::size_t DeviceIndex);
+    std::string GetStatus();
+    float GetSpeed();
+    playback_mode GetMode();
+    void CreateCaptureSession(FileWrapper* Wrapper);
+    void StartCaptureSession();
+    void StopCaptureSession();
+    void SetPlaybackMode(playback_mode Mode, float Speed);
+    bool WaitForSessionEnd(uint64_t Timeout);
+
+private:
+    // global
+    int Port = -1;
+    nodeid_t Node = (nodeid_t)-1;
+    octlet_t UUID = (octlet_t)-1;
+
+    // avc1394
+    raw1394handle_t CtlHandle = nullptr;
+
+    // iec61883
+    int InPlug = -1;
+    int OutPlug = -1;
+    int Channel = -1;
+    int Bandwidth = -1;
+    iec61883_dv_fb_t Frame = nullptr;
+    raw1394handle_t CaptureHandle = nullptr;
+    std::thread* Raw1394PoolingThread = nullptr;
+    std::atomic_bool Raw1394PoolingThread_Terminate { false };
+};
