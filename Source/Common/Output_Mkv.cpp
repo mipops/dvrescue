ded0b09d961406bcb3ca73c1bde0dff57ed44bdc
Jerome Martinez <jerome@mediaarea.net>
2023-11-30 13:53:19 +0100

    Add top level crc's for mkv captures

--- Source/Common/Output_Mkv.cpp
+++ Source/Common/Output_Mkv.cpp
@@ -15,8 +15,13 @@ static const unsigned char matroska_writer_header[]                 = { 0x1A, 0x
 static const unsigned char matroska_writer_header_seek[]            = { 0x4D, 0xBB, 0x92, 0x53, 0xAB, 0x84, 0x1C, 0x53, 0xBB, 0x6B, 0x53, 0xAC, 0x88 };
 static const unsigned char matroska_writer_header_timecode[]        = { 0x41, 0xE4, 0x8A, 0x41, 0xE7, 0x83, 0x31, 0x32, 0x31, 0x41, 0xF0, 0x81, 0x79 };
 static const unsigned char matroska_writer_header_texttrack[]       = { 0xAE, 0xA1, 0xD7, 0x81, 0x03, 0x73, 0xC5, 0x81, 0x03, 0xEC, 0x85, 0x00, 0x00, 0x00, 0x00, 0x00, 0x83, 0x81, 0x11, 0x86, 0x8B, 0x53, 0x5F, 0x54, 0x45, 0x58, 0x54, 0x2F, 0x55, 0x54, 0x46, 0x38, 0x9C, 0x81, 0x00 };
+static const size_t matroska_writer_header_seek_crc_beg             = 0x0039;
 static const size_t matroska_writer_header_seek_offset              = 0x006A;
+static const size_t matroska_writer_header_seek_crc_end             = 0x007F;
+static const size_t matroska_writer_header_info_crc_beg             = 0x00DA;
 static const size_t matroska_writer_header_totalduration_offset     = 0x0118;
+static const size_t matroska_writer_header_info_crc_end             = 0x0123;
+static const size_t matroska_writer_header_tracks_crc_beg           = 0x0129;
 static const size_t matroska_writer_header_defaultduration_offset   = 0x015B;
 static const size_t matroska_writer_header_width_offset             = 0x0174;
 static const size_t matroska_writer_header_height_offset            = 0x0178;
@@ -25,6 +30,7 @@ static const size_t matroska_writer_header_avi_width_offset         = 0x019F;
 static const size_t matroska_writer_header_avi_height_offset        = 0x01A3;
 static const size_t matroska_writer_header_timecode_offset          = 0x01C3;
 static const size_t matroska_writer_header_texttrack_offset         = 0x021A;
+static const size_t matroska_writer_header_tracks_crc_end           = 0x023D;
 
 static void store_b1(char*& buffer, int value)
 {
@@ -99,17 +105,121 @@ static void store_bf8(char*& buffer, double value)
     store_b8(buffer, *fake_int);
 }
 
+//---------------------------------------------------------------------------
+// CRC_32_Table (Little Endian bitstream)
+// The CRC in use is the IEEE-CRC-32 algorithm as used in the ISO 3309 standard and in section 8.1.1.6.2 of ITU-T recommendation V.42, with initial value of 0xFFFFFFFF. The CRC value MUST be computed on a little endian bitstream and MUST use little endian storage.
+// A CRC is computed like this:
+// Init: unsigned crc32 ^= 0;
+// for each data byte do
+//     crc32=(crc32>>8) ^ matroska_crc32_table[(crc32&0xFF)^*Current++];
+// End: crc32 ^= 0;
+static const unsigned matroska_crc32_table[256] =
+{
+    0x00000000, 0x77073096, 0xEE0E612C, 0x990951BA,
+    0x076DC419, 0x706AF48F, 0xE963A535, 0x9E6495A3,
+    0x0EDB8832, 0x79DCB8A4, 0xE0D5E91E, 0x97D2D988,
+    0x09B64C2B, 0x7EB17CBD, 0xE7B82D07, 0x90BF1D91,
+    0x1DB71064, 0x6AB020F2, 0xF3B97148, 0x84BE41DE,
+    0x1ADAD47D, 0x6DDDE4EB, 0xF4D4B551, 0x83D385C7,
+    0x136C9856, 0x646BA8C0, 0xFD62F97A, 0x8A65C9EC,
+    0x14015C4F, 0x63066CD9, 0xFA0F3D63, 0x8D080DF5,
+    0x3B6E20C8, 0x4C69105E, 0xD56041E4, 0xA2677172,
+    0x3C03E4D1, 0x4B04D447, 0xD20D85FD, 0xA50AB56B,
+    0x35B5A8FA, 0x42B2986C, 0xDBBBC9D6, 0xACBCF940,
+    0x32D86CE3, 0x45DF5C75, 0xDCD60DCF, 0xABD13D59,
+    0x26D930AC, 0x51DE003A, 0xC8D75180, 0xBFD06116,
+    0x21B4F4B5, 0x56B3C423, 0xCFBA9599, 0xB8BDA50F,
+    0x2802B89E, 0x5F058808, 0xC60CD9B2, 0xB10BE924,
+    0x2F6F7C87, 0x58684C11, 0xC1611DAB, 0xB6662D3D,
+    0x76DC4190, 0x01DB7106, 0x98D220BC, 0xEFD5102A,
+    0x71B18589, 0x06B6B51F, 0x9FBFE4A5, 0xE8B8D433,
+    0x7807C9A2, 0x0F00F934, 0x9609A88E, 0xE10E9818,
+    0x7F6A0DBB, 0x086D3D2D, 0x91646C97, 0xE6635C01,
+    0x6B6B51F4, 0x1C6C6162, 0x856530D8, 0xF262004E,
+    0x6C0695ED, 0x1B01A57B, 0x8208F4C1, 0xF50FC457,
+    0x65B0D9C6, 0x12B7E950, 0x8BBEB8EA, 0xFCB9887C,
+    0x62DD1DDF, 0x15DA2D49, 0x8CD37CF3, 0xFBD44C65,
+    0x4DB26158, 0x3AB551CE, 0xA3BC0074, 0xD4BB30E2,
+    0x4ADFA541, 0x3DD895D7, 0xA4D1C46D, 0xD3D6F4FB,
+    0x4369E96A, 0x346ED9FC, 0xAD678846, 0xDA60B8D0,
+    0x44042D73, 0x33031DE5, 0xAA0A4C5F, 0xDD0D7CC9,
+    0x5005713C, 0x270241AA, 0xBE0B1010, 0xC90C2086,
+    0x5768B525, 0x206F85B3, 0xB966D409, 0xCE61E49F,
+    0x5EDEF90E, 0x29D9C998, 0xB0D09822, 0xC7D7A8B4,
+    0x59B33D17, 0x2EB40D81, 0xB7BD5C3B, 0xC0BA6CAD,
+    0xEDB88320, 0x9ABFB3B6, 0x03B6E20C, 0x74B1D29A,
+    0xEAD54739, 0x9DD277AF, 0x04DB2615, 0x73DC1683,
+    0xE3630B12, 0x94643B84, 0x0D6D6A3E, 0x7A6A5AA8,
+    0xE40ECF0B, 0x9309FF9D, 0x0A00AE27, 0x7D079EB1,
+    0xF00F9344, 0x8708A3D2, 0x1E01F268, 0x6906C2FE,
+    0xF762575D, 0x806567CB, 0x196C3671, 0x6E6B06E7,
+    0xFED41B76, 0x89D32BE0, 0x10DA7A5A, 0x67DD4ACC,
+    0xF9B9DF6F, 0x8EBEEFF9, 0x17B7BE43, 0x60B08ED5,
+    0xD6D6A3E8, 0xA1D1937E, 0x38D8C2C4, 0x4FDFF252,
+    0xD1BB67F1, 0xA6BC5767, 0x3FB506DD, 0x48B2364B,
+    0xD80D2BDA, 0xAF0A1B4C, 0x36034AF6, 0x41047A60,
+    0xDF60EFC3, 0xA867DF55, 0x316E8EEF, 0x4669BE79,
+    0xCB61B38C, 0xBC66831A, 0x256FD2A0, 0x5268E236,
+    0xCC0C7795, 0xBB0B4703, 0x220216B9, 0x5505262F,
+    0xC5BA3BBE, 0xB2BD0B28, 0x2BB45A92, 0x5CB36A04,
+    0xC2D7FFA7, 0xB5D0CF31, 0x2CD99E8B, 0x5BDEAE1D,
+    0x9B64C2B0, 0xEC63F226, 0x756AA39C, 0x026D930A,
+    0x9C0906A9, 0xEB0E363F, 0x72076785, 0x05005713,
+    0x95BF4A82, 0xE2B87A14, 0x7BB12BAE, 0x0CB61B38,
+    0x92D28E9B, 0xE5D5BE0D, 0x7CDCEFB7, 0x0BDBDF21,
+    0x86D3D2D4, 0xF1D4E242, 0x68DDB3F8, 0x1FDA836E,
+    0x81BE16CD, 0xF6B9265B, 0x6FB077E1, 0x18B74777,
+    0x88085AE6, 0xFF0F6A70, 0x66063BCA, 0x11010B5C,
+    0x8F659EFF, 0xF862AE69, 0x616BFFD3, 0x166CCF45,
+    0xA00AE278, 0xD70DD2EE, 0x4E048354, 0x3903B3C2,
+    0xA7672661, 0xD06016F7, 0x4969474D, 0x3E6E77DB,
+    0xAED16A4A, 0xD9D65ADC, 0x40DF0B66, 0x37D83BF0,
+    0xA9BCAE53, 0xDEBB9EC5, 0x47B2CF7F, 0x30B5FFE9,
+    0xBDBDF21C, 0xCABAC28A, 0x53B39330, 0x24B4A3A6,
+    0xBAD03605, 0xCDD70693, 0x54DE5729, 0x23D967BF,
+    0xB3667A2E, 0xC4614AB8, 0x5D681B02, 0x2A6F2B94,
+    0xB40BBE37, 0xC30C8EA1, 0x5A05DF1B, 0x2D02Ef8D,
+};
+
+static unsigned matroska_crc32_compute(const char* buffer, size_t size)
+{
+    unsigned crc32 = 0xFFFFFFFF;
+    auto cur = (unsigned char*)buffer;
+    auto end = cur + size;
+    while (cur < end)
+        crc32 = (crc32 >> 8) ^ matroska_crc32_table[(crc32 & 0xFF) ^ *cur++];
+    crc32 ^= 0xFFFFFFFF;
+    return crc32;
+}
+
+static void matroska_crc32_store(char* buffer, size_t size)
+{
+    if (size < 6)
+        return;
+    auto crc32 = matroska_crc32_compute(buffer + 6, size - 6);
+    store_b1(buffer, 0xBF); // CRC-32
+    store_b1(buffer, 0x84);
+    store_l4(buffer, crc32);
+}
+
 matroska_writer::matroska_writer(std::ostream* output, int width, int height, int framerate_num, int framerate_den, bool has_timecode)
     : width(width)
     , height(height)
     , framerate_num(framerate_num)
     , framerate_den(framerate_den)
     , has_timecode(has_timecode)
+    , buffer(nullptr)
+    , buffer_size(0)
     , frame_number(0)
     , output_size(sizeof(matroska_writer_header) - 0x34)
     , output(output)
 {
     write_header();
+}
+
+matroska_writer::~matroska_writer()
+{
+    delete[] buffer;
 }
 
 void matroska_writer::write_header()
@@ -153,29 +263,42 @@ void matroska_writer::write_header()
         memcpy(cur, matroska_writer_header_texttrack, sizeof(matroska_writer_header_texttrack));
     }
 
+    matroska_crc32_store(buffer + matroska_writer_header_seek_crc_beg, matroska_writer_header_seek_crc_end - matroska_writer_header_seek_crc_beg);
+    matroska_crc32_store(buffer + matroska_writer_header_info_crc_beg, matroska_writer_header_info_crc_end - matroska_writer_header_info_crc_beg);
+    matroska_crc32_store(buffer + matroska_writer_header_tracks_crc_beg, matroska_writer_header_tracks_crc_end - matroska_writer_header_tracks_crc_beg);
     output->write(buffer, sizeof(matroska_writer_header));
 }
 
 void matroska_writer::write_frame(const char* video_buffer, int video_size, const char* audio_buffer, int audio_size, timecode_struct timecode)
 {
+    // Cues
     double timecode_ms = (double)frame_number * 1000 * framerate_den / framerate_num + 0.5;
     auto timecode_s = timecode_ms / 1000;
     if (timecode_s >= cues.size())
         cues.push_back({ (unsigned long long)timecode_ms, output_size });
-    int total_size = 4 + 1 + 1 + (1 + 4 + 1 + 2 + 1) * 2 + video_size + audio_size;
+    
+    // Buffer
+    int total_size = 6 + 4 + 1 + 1 + (1 + 4 + 1 + 2 + 1) * 2 + video_size + audio_size;
     if (timecode.is_valid())
         total_size += 1 + 4 + 18 + (1 + 4 + 1 + 2 + 1) * 1 + 11;
-    char buffer[0x20];
-    char* cur;
-    size_t size;
+    auto total_size_with_header = 8 + total_size;
+    if (buffer_size < total_size_with_header)
+    {
+        buffer_size = total_size_with_header;
+        delete[] buffer;
+        buffer = new char[buffer_size];
+    }
+    auto cur = buffer;
 
-    // Video
-    cur = buffer;
+    // Header
     store_b4(cur, 0x1F43B675); // Cluster
     store_e4(cur, total_size);
+    cur += 6; // CRC-32
     store_b1(cur, 0xE7); // TimeStamp
     store_b1(cur, 0x84);
-    store_b4(cur, timecode_ms);
+    store_b4(cur, (int)(timecode_ms+0.5));
+
+    // Video
     if (timecode.is_valid())
     {
         store_b1(cur, 0xA0); // BlockGroup
@@ -188,13 +311,9 @@ void matroska_writer::write_frame(const char* video_buffer, int video_size, cons
     }
     store_e4(cur, 1 + 2 + 1 + video_size);
     store_b4(cur, 0x81000080); // TrackNumber + relative TimeStamp + Flags
-    size = cur - buffer;
-    output->write(buffer, size);
-    output_size += size;
-    output->write(video_buffer, video_size);
-    output_size += video_size;
+    memcpy(cur, video_buffer, video_size);
+    cur += video_size;
 
-    cur = buffer;
     if (timecode.is_valid())
     {
         unsigned long long timecode_value = ((unsigned long long)(((timecode.hours   / 10) & 3)) << 56)
@@ -210,14 +329,13 @@ void matroska_writer::write_frame(const char* video_buffer, int video_size, cons
         store_b8(cur, 0x8FA68DEE8179A588LL); // BlockMore size + BlockAddID + BlockAdditional
         store_b8(cur, timecode_value);
     }
+    
+    // Audio
     store_b1(cur, 0xA3); // SimpleBlock
     store_e4(cur, 1 + 2 + 1 + audio_size);
     store_b4(cur, 0x82000080); // TrackNumber + relative TimeStamp + Flags
-    size = cur - buffer;
-    output->write(buffer, size);
-    output_size += size;
-    output->write(audio_buffer, audio_size);
-    output_size += audio_size;
+    memcpy(cur, audio_buffer, audio_size);
+    cur += audio_size;
 
     if (timecode.is_valid())
     {
@@ -233,17 +351,17 @@ void matroska_writer::write_frame(const char* video_buffer, int video_size, cons
         text_buffer[ 9] += (timecode.frames  / 10) & 3;
         text_buffer[10] +=  timecode.frames  % 10;
 
-        cur = buffer;
         store_b1(cur, 0xA3); // SimpleBlock
         store_e4(cur, 1 + 2 + 1 + 11);
         store_b4(cur, 0x83000000); // TrackNumber + relative TimeStamp + Flags
-        size = cur - buffer;
-        output->write(buffer, size);
-        output_size += size;
-        output->write(text_buffer, 11);
-        output_size += 11;
+        memcpy(cur, text_buffer, 11);
+        cur += 11;
     }
 
+    auto size = cur - buffer;
+    matroska_crc32_store(buffer + 8, total_size);
+    output->write(buffer, size);
+    output_size += size;
     frame_number++;
 
     cerr << "\33[2K\rCapture frame " << frame_number << ", press " << (InControl ? "q" : "ctrl+c") << " to stop.";
@@ -255,11 +373,11 @@ void matroska_writer::close(std::ofstream* output)
     char* cur;
 
     // Cues
-    size_t cues_size = 27 * cues.size();
+    int cues_size = 6 + 27 * (int)cues.size();
     cur = buffer;
     store_b4(cur, 0x1C53BB6B); // Cues
     store_e4(cur, cues_size);
-    cues_size += 8;
+    cur += 6; // CRC-32
     for (const auto& cue : cues)
     {
         store_b4(cur, 0xBB99B388); // CuePoint + CueTime
@@ -279,6 +397,7 @@ void matroska_writer::close(std::ofstream* output)
     if (cur > buffer)
     {
         auto size = cur - buffer;
+        matroska_crc32_store(buffer + 8, cues_size);
         output->write(buffer, size);
         output_size += size;
     }
